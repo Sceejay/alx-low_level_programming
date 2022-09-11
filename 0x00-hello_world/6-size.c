0beb780dbce67938aaf9bbcf7b16648e256ce485
Sceejay <chinazak526@gmail.com>
2022-09-11 11:41:22 -0700

    Display size of all c data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: returns 0 Always unless specified not to
+ */
+int main(void)
+{
+	char chars;
+	int integer;
+	unsigned int long_integers;
+	float floating_point;
+	long long int integers;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(chars));
+	printf("Size of an int: %zu byte(s)\n", sizeof(integer));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long_integers));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(integers));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floating_point));
+	return (0);
+}
